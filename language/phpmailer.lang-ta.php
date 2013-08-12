987bc89aebfc73b1cd471d37ff817ae7aa15f74c
Sajeepan <naduvaa@gmail.com>
2013-08-12 16:15:21 +0530

    phpmailer.lang-ta.php
    Tamil version

---
+++ language/phpmailer.lang-ta.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * PHPMailer language file.
+ * Tamil Version
+ * by : Sajeepan yogenthiran <naduvaa@gmail.com>
+ */
+
+$PHPMAILER_LANG = array();
+
+$PHPMAILER_LANG['provide_address']      = 'நீங்கள் குறைந்தது ஒரு பெறுநர் மின்னஞ்சல் முகவரி வழங்க வேண்டும்.';
+$PHPMAILER_LANG['mailer_not_supported'] = ' mailer உடைய ஆதரவு இல்லை.';
+$PHPMAILER_LANG['execute']              = 'இயக்க முடியவில்லை: ';
+$PHPMAILER_LANG['instantiate']          = 'Could not instantiate mail function.';
+$PHPMAILER_LANG['authenticate']         = 'SMTP வழு: அங்கீகரிக்க முடியவில்லை.';
+$PHPMAILER_LANG['from_failed']          = 'பின்வரும் முகவரிக்கு அனுப்ப முடியாதுள்ளது: ';
+$PHPMAILER_LANG['recipients_failed']    = 'SMTP வழு: பின்வரும் பெறுநர்களுக்கு அனுப்ப முடியாதுள்ளது: ';
+$PHPMAILER_LANG['data_not_accepted']    = 'SMTP வழு: தரவு ஏற்றுக்கொள்ளப்பட்டது.';
+$PHPMAILER_LANG['connect_host']         = 'SMTP வழு: SMTP புரவலனை இணைக்க முடியவில்லை.';
+$PHPMAILER_LANG['file_access']          = 'கோப்பு அணுக முடியவில்லை: ';
+$PHPMAILER_LANG['file_open']            = 'கோப்பு வழு: கோப்பை திறக்க முடியவில்லை: ';
+$PHPMAILER_LANG['encoding']             = 'அறியப்படாத குறிமுறையாக்கம்: ';
+$PHPMAILER_LANG['signing']              = 'Signing Error: ';
+$PHPMAILER_LANG['smtp_error']           = 'SMTP சேவையகத்திலிருந்து வழு: ';
+?>
